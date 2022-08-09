1fccabcec73595bd68b62cb3d66806142c9f1c71
EC2 Default User <ec2-user@ip-172-31-12-24.us-west-1.compute.internal>
2022-08-09 04:23:10 +0000

    recipegram

---
+++ app/models/recipe.rb
@@ -0,0 +1,12 @@
+class Recipe < ApplicationRecord
+  
+  belongs_to :user
+  attachment :image
+  
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :image
+  end
+  
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f5655ab248492d248e6ced5a485a1f538f3b76a4c8626204eef7ab2f4b75d998c658b202a61b94cbb73f095a247f1356d6b609ccf9fdf63dde8d03e783bcd420'
